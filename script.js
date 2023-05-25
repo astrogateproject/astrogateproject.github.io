403edf5889fe994e6c33461f982d3001e94f7ced
javorao <43182900+javorao@users.noreply.github.com>
2023-05-25 23:03:16 +0200

    změna na novou verzi

---
+++ script.js
@@ -0,0 +1,104 @@
+// ********************
+// BACKGROUND PARALAX SCRIPT
+// ********************
+
+    const speed = 0.7;
+  
+    window.addEventListener('scroll', function() {
+      const yPos = (window.pageYOffset * speed);
+      document.body.style.backgroundPosition = `center ${yPos}px`;
+    });
+
+
+
+
+// ********************
+// WINDOW SIZES SCRIPTS
+// ********************
+
+
+    const menu = document.getElementById("menu");
+
+    var maxColWidth = (getComputedStyle(document.documentElement).getPropertyValue('--maxcolw'));
+    //convert to integer
+    maxColWidth = maxColWidth.substring(0, maxColWidth.length - 2);
+    maxColWidth = parseInt(maxColWidth);
+
+    var minColWidth = (getComputedStyle(document.documentElement).getPropertyValue('--mincolw'));
+    //convert to integer
+    minColWidth = minColWidth.substring(0, minColWidth.length - 2);
+    minColWidth = parseInt(minColWidth);
+
+    var menuWidth = 300;
+
+    var astrogateMinSize = 128;
+
+// HEADER SCRIPT
+
+    const astrogate = document.getElementById("astrogate");
+    const title = document.getElementById("title");
+
+
+    //RESIZES astrogate based on window width
+    if (((window.innerWidth - maxColWidth)/2 - 9) > astrogateMinSize) {
+        astrogate.style.setProperty('width', ((window.innerWidth - maxColWidth)/2 - 9 + "px"));
+    }
+
+    else {
+        astrogate.style.setProperty('width', astrogateMinSize + "px");
+    }
+
+    window.addEventListener('resize', function(event) {
+        if (((window.innerWidth - maxColWidth)/2 - 9) > astrogateMinSize) {
+            astrogate.style.setProperty('width', ((window.innerWidth - maxColWidth)/2 - 9 + "px"));
+        }
+
+        else {
+            astrogate.style.setProperty('width', astrogateMinSize + "px");
+        }
+    });
+
+    //RESIZES title bar based on window width
+    window.addEventListener('resize', function(event) {
+        if (((window.innerWidth - maxColWidth)/2 - 9) >= astrogateMinSize) {
+            title.style.setProperty('width', (maxColWidth + "px"));
+        }
+
+        else {
+            title.style.setProperty('width', (window.innerWidth - ((window.innerWidth - maxColWidth)/2) ) - astrogateMinSize + "px");
+
+        }
+    });
+ 
+// SIDE MENU SCIRPTS
+
+    // RESIZES side menu based on the window width
+    document.documentElement.style.setProperty('--menusize', window.innerWidth < minColWidth ? '100%' : (menuWidth + "px"));
+
+    window.addEventListener('resize', function(event) {
+        if (window.innerWidth < minColWidth) {
+            document.documentElement.style.setProperty('--menusize', "100%");
+        } 
+
+        else if (window.innerWidth >= minColWidth) {
+            document.documentElement.style.setProperty('--menusize', (menuWidth + "px"));
+        }
+    });
+
+    //checks if the menu should load collapsed on small screens
+        if (window.innerWidth < (maxColWidth + (2 * menuWidth))) {
+            menu.classList.add("closed");
+
+        } else {
+      menu.classList.remove("closed");
+    }
+
+    
+    // This is for the button on the side menu to close and open it
+    function toggleMenu() {
+        if (menu.classList.contains("closed")) {
+            menu.classList.remove("closed");
+        }  else {
+            menu.classList.add("closed");
+        }
+    }
